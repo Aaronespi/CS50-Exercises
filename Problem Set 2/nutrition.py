e122d0abcb9b5f528932e48f2c7ddc60e97f8195
Aaron Espinosa <aaron.espinosa93@gmail.com>
2024-02-15 09:02:43 +0100

    5th problem solved
    Given a fruit, search on a dictionary and, if there is a match, print his calories

---
+++ Problem Set 2/nutrition.py
@@ -0,0 +1,33 @@
+def main():
+    #takes an input and make it lowercase
+    fruit = input("Item: ").lower()
+    calorieMeter(fruit)
+
+def calorieMeter(s):
+    fruits = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew melon": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+    }
+#checks if the input it's in the dictionary and if it is, print his calories
+    if s in fruits:
+        print("Calories: ", fruits[s])
+
+main()
